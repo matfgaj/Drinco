abf5820b30a207be31cbd8a3aef236eb410d891f
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-04 23:59:29 +0200

    store update

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createStore } from "@reduxjs/toolkit";
 import drinks from "./drinks/duck/reducers";
 import display from "./displayedPage/duck/reducers";
+
 
 export default configureStore({
   reducer: {
